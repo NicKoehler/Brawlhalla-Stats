74312979d03d302bdaccd1b1f1700008ddff68c4
NicKoehler <grillinicola@proton.me>
2024-11-27 19:09:18 +0100

    Implement deep link

--- app/src/main/java/com/nickoehler/brawlhalla/core/domain/util/NetworkError.kt
+++ app/src/main/java/com/nickoehler/brawlhalla/core/domain/util/NetworkError.kt
@@ -3,6 +3,7 @@ package com.nickoehler.brawlhalla.core.domain.util
 enum class NetworkError : Error {
     REQUEST_TIMEOUT,
     TOO_MANY_REQUESTS,
+    NOT_FOUND,
     NO_INTERNET,
     SERVER_ERROR,
     SERIALIZATION,
--- app/src/main/java/com/nickoehler/brawlhalla/core/presentation/util/NavDestinationToString.kt
+++
@@ -1,7 +0,0 @@
-package com.nickoehler.brawlhalla.core.presentation.util
-
-import androidx.navigation.NavDestination
-
-fun NavDestination.toNavString(): String {
-    return this.route?.split(".")?.last().toString()
-}
