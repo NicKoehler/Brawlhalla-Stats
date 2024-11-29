faf2ba639619d87d7ebdf036efc0f1e835ed79d3
NicKoehler <grillinicola@proton.me>
2024-11-29 12:12:22 +0100

    Fix nav bar icons and add labels

--- app/src/main/java/com/nickoehler/brawlhalla/ui/Screens.kt
+++ app/src/main/java/com/nickoehler/brawlhalla/ui/Screens.kt
@@ -3,17 +3,33 @@ package com.nickoehler.brawlhalla.ui
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.People
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 import kotlinx.serialization.Serializable
 
-enum class Screens(val title: String, val icon: ImageVector, val route: Route) {
-    LEGEND(title = "Legends", icon = Icons.Default.People, route = Route.Legend()),
-    SEARCH(title = "Search", icon = Icons.Default.Search, route = Route.Search);
+enum class Screens(
+    val title: String,
+    val route: Route,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+) {
+    LEGEND(
+        title = "Legends",
+        route = Route.Legend(),
+        selectedIcon = Icons.Default.People,
+        unselectedIcon = Icons.Outlined.People
+    ),
+    SEARCH(
+        title = "Search",
+        route = Route.Search,
+        selectedIcon = Icons.Default.Search,
+        unselectedIcon = Icons.Outlined.Search
+    );
 }
 
 
 sealed interface Route {
-
 
     @Serializable
     data object Search : Route
