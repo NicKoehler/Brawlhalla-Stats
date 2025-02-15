164fff1e5f31a313683bdeeb9a59eb7540a27c3c
NicKoehler <grillinicola@proton.me>
2025-02-15 13:06:01 +0100

    fix: Improve LegendRankingItem

--- app/src/main/java/com/nickoehler/brawlhalla/ranking/presentation/components/LegendRankingItem.kt
+++ app/src/main/java/com/nickoehler/brawlhalla/ranking/presentation/components/LegendRankingItem.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -16,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,6 +32,7 @@ import com.nickoehler.brawlhalla.core.presentation.components.LegendImage
 import com.nickoehler.brawlhalla.ranking.presentation.components.ranking_card.RankWinRateRow
 import com.nickoehler.brawlhalla.ranking.presentation.components.ranking_card.TierBox
 import com.nickoehler.brawlhalla.ranking.presentation.models.RankingLegendUi
+import com.nickoehler.brawlhalla.ranking.presentation.models.toLocalizedString
 import com.nickoehler.brawlhalla.ranking.presentation.models.toRankingLegendUi
 import com.nickoehler.brawlhalla.ranking.presentation.screens.rankingDetailSample
 import com.nickoehler.brawlhalla.ui.theme.BrawlhallaTheme
@@ -90,8 +95,10 @@ fun LegendRankingItemDetail(
 
     Column(
         modifier = modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+
         Text(
             legend.legendNameKey,
             fontSize = 30.sp,
@@ -99,6 +106,7 @@ fun LegendRankingItemDetail(
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
+        TierBox(legend.rating, legend.tier)
 
         if (legend.winRate != null) {
             RankWinRateRow(legend.winRate)
@@ -110,20 +118,16 @@ fun LegendRankingItemDetail(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
+            item { CustomRankingField(R.string.rating, legend.rating.formatted, background) }
             item {
                 CustomRankingField(
-                    R.string.games,
-                    legend.games.formatted,
+                    R.string.peakRating,
+                    legend.peakRating.formatted,
                     background
                 )
             }
-            item {
-                CustomRankingField(
-                    R.string.wins,
-                    legend.wins.formatted,
-                    background
-                )
-            }
+            item { CustomRankingField(R.string.games, legend.games.formatted, background) }
+            item { CustomRankingField(R.string.wins, legend.wins.formatted, background) }
         }
     }
 }
