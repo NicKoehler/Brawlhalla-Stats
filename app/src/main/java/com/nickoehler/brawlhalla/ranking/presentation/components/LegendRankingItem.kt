389a5f3ca0d7270143c99afecc57e86d19f15f75
NicKoehler <grillinicola@proton.me>
2025-02-21 17:26:26 +0100

    feat: Add italian

--- app/src/main/java/com/nickoehler/brawlhalla/ranking/presentation/components/LegendRankingItem.kt
+++ app/src/main/java/com/nickoehler/brawlhalla/ranking/presentation/components/LegendRankingItem.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -19,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +28,6 @@ import com.nickoehler.brawlhalla.core.presentation.components.LegendImage
 import com.nickoehler.brawlhalla.ranking.presentation.components.ranking_card.RankWinRateRow
 import com.nickoehler.brawlhalla.ranking.presentation.components.ranking_card.TierBox
 import com.nickoehler.brawlhalla.ranking.presentation.models.RankingLegendUi
-import com.nickoehler.brawlhalla.ranking.presentation.models.toLocalizedString
 import com.nickoehler.brawlhalla.ranking.presentation.models.toRankingLegendUi
 import com.nickoehler.brawlhalla.ranking.presentation.screens.rankingDetailSample
 import com.nickoehler.brawlhalla.ui.theme.BrawlhallaTheme
