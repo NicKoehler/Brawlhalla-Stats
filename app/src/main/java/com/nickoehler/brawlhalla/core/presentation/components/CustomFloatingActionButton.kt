ee2a477b9ef1d8193bf9ce8850260feefe0500ed
NicKoehler <grillinicola@proton.me>
2025-09-09 09:58:17 +0200

    fix: Animation CustomFloatingActionButton

--- app/src/main/java/com/nickoehler/brawlhalla/core/presentation/components/CustomFloatingActionButton.kt
+++ app/src/main/java/com/nickoehler/brawlhalla/core/presentation/components/CustomFloatingActionButton.kt
@@ -1,10 +1,6 @@
 package com.nickoehler.brawlhalla.core.presentation.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowUpward
@@ -12,12 +8,15 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import kotlinx.coroutines.launch
 
 @Composable
@@ -26,24 +25,32 @@ fun CustomFloatingActionButton(
 ) {
     val coroutineScope = rememberCoroutineScope()
     var visibleIndexIsNotZero by remember { mutableStateOf(false) }
+
+    val isVisible by remember {
+        derivedStateOf {
+            scrollState.lastScrolledBackward && visibleIndexIsNotZero
+        }
+    }
+
     LaunchedEffect(scrollState) {
         snapshotFlow { scrollState.firstVisibleItemIndex }
             .collect {
-                visibleIndexIsNotZero = it != 0;
+                visibleIndexIsNotZero = it != 0
             }
     }
+    val translationY by animateFloatAsState(if (isVisible) 0f else 500f)
 
-    AnimatedVisibility(
-        scrollState.lastScrolledBackward && visibleIndexIsNotZero,
-        enter = fadeIn() + slideInVertically { it },
-        exit = fadeOut() + slideOutVertically { it }
-    ) {
-        FloatingActionButton(onClick = {
+    FloatingActionButton(
+        modifier = Modifier.graphicsLayer(
+            translationY = translationY
+        ),
+        onClick = {
             coroutineScope.launch {
                 scrollState.animateScrollToItem(0)
             }
-        }) {
-            Icon(Icons.Default.ArrowUpward, null)
-        }
+        },
+    ) {
+        Icon(Icons.Default.ArrowUpward, null)
     }
+
 }
