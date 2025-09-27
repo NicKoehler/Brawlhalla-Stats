69116615acb933fa411cd919dc05582bbddbdf86
NicKoehler <grillinicola@proton.me>
2025-09-27 15:52:09 +0200

    fix: readd preview dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,6 +129,7 @@ dependencies {
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
 }
 
